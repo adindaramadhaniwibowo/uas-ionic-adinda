09b34a8ace105c0d65e26479ffb0a885bcf83d27
adindaramdhaniwibowo <syauqi.djohan@student.umn.ac.id>
2020-12-18 12:46:21 +0700

    UAS

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBmt2wh8ofJvzeQGcBebXbKOd5kGABG-wA",
+    authDomain: "uas-ionic-dane-2020.firebaseapp.com",
+    projectId: "uas-ionic-dane-2020",
+    storageBucket: "uas-ionic-dane-2020.appspot.com",
+    messagingSenderId: "263540770335",
+    appId: "1:263540770335:web:fd61c414db623fbaa1372a",
+    measurementId: "G-22TYE0JJGQ"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
